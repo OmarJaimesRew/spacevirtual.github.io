a6649572ffa14e0b570fc1025493089a4758937f
OmarJaimesRew <47762753+OmarJaimesRew@users.noreply.github.com>
2025-06-06 21:25:37 -0600

    Add gaze delay and side text

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 AFRAME.registerComponent('info-listener', {
   schema: { text: { type: 'string' } },
   init: function () {
-    var infoPanel = document.querySelector('#infoPanel');
-    var infoText = document.querySelector('#infoText');
-    this.el.addEventListener('mouseenter', () => {
-      infoText.setAttribute('value', this.data.text);
-      infoPanel.setAttribute('visible', true);
+    var textEl = this.el.querySelector('.info-text');
+    if (textEl) {
+      textEl.setAttribute('value', this.data.text);
+    }
+    this.el.addEventListener('click', () => {
+      if (textEl) {
+        textEl.setAttribute('visible', true);
+      }
     });
     this.el.addEventListener('mouseleave', () => {
-      infoPanel.setAttribute('visible', false);
+      if (textEl) {
+        textEl.setAttribute('visible', false);
+      }
     });
   }
 });
