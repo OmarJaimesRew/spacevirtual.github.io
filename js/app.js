27b06a5cf3fd71201c0fd4cf2088ecaa2deab4df
OmarJaimesRew <47762753+OmarJaimesRew@users.noreply.github.com>
2025-06-06 21:52:15 -0600

    Fix cursor progress to restart on hover

--- js/app.js
+++ js/app.js
@@ -17,3 +17,52 @@ AFRAME.registerComponent('info-listener', {
     });
   }
 });
+
+AFRAME.registerComponent('cursor-progress', {
+  init: function () {
+    const progressRing = this.el.querySelector('#cursorProgress');
+    if (!progressRing) { return; }
+
+    let intersected = false;
+
+    const start = () => {
+      progressRing.removeAttribute('animation__fill');
+      progressRing.removeAttribute('animation__reset');
+      progressRing.setAttribute('visible', true);
+      progressRing.setAttribute('animation__fill', {
+        property: 'geometry.thetaLength',
+        from: 0,
+        to: 360,
+        dur: 3000,
+        easing: 'linear'
+      });
+    };
+
+    const reset = () => {
+      progressRing.removeAttribute('animation__fill');
+      progressRing.setAttribute('animation__reset', {
+        property: 'geometry.thetaLength',
+        to: 0,
+        dur: 0
+      });
+      progressRing.setAttribute('visible', false);
+    };
+
+    this.el.addEventListener('raycaster-intersection', () => {
+      intersected = true;
+      start();
+    });
+
+    this.el.addEventListener('raycaster-intersection-cleared', () => {
+      intersected = false;
+      reset();
+    });
+
+    this.el.addEventListener('click', () => {
+      reset();
+      if (intersected) {
+        start();
+      }
+    });
+  }
+});
